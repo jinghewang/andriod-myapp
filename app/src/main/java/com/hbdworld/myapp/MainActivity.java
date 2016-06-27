02b46ad7a392e192529d4cc6cd77b35e1e8a47a5
jinghewang <jinghewang@163.com>
2016-06-27 21:24:19 +0800

    html 调用 app 并传递参数

--- app/src/main/java/com/hbdworld/myapp/MainActivity.java
+++ app/src/main/java/com/hbdworld/myapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.hbdworld.myapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //---
+        LinearLayout layout = (LinearLayout)this.findViewById(R.id.root);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        String scheme = intent.getScheme();
+        if (scheme != null){
+            TextView textView = new TextView(this);
+            textView.setText("key:" + "scheme" + "   value:" + scheme);
+            layout.addView(textView);
+
+            Set<String> sets = uri.getQueryParameterNames();
+            for (String set : sets) {
+                textView = new TextView(this);
+                textView.setText("key:" + set + "   value:" + uri.getQueryParameter(set));
+                layout.addView(textView);
+            }
+        }
     }
 }
